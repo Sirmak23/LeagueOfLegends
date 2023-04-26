e0d1b9663dec8f67fd17753826a2f767908ba747
süleyman <s.irmk9@gmail.com>
2023-04-26 17:59:56 +0300

    tekrar gözden geçirildi

---
+++ app/src/main/java/com/irmak/leagueoflegends/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.irmak.leagueoflegends.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.irmak.leagueoflegends.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

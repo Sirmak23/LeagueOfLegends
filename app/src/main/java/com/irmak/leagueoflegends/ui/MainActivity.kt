15a17b94cd842b09e5f6756c89c68595255391c2
süleyman <s.irmk9@gmail.com>
2023-04-26 17:45:28 +0300

    created RetrofitClient <3

--- app/src/main/java/com/irmak/leagueoflegends/ui/MainActivity.kt
+++ app/src/main/java/com/irmak/leagueoflegends/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.irmak.leagueoflegends
+package com.irmak.leagueoflegends.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.irmak.leagueoflegends.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
